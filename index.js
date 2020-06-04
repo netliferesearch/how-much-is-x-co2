8575d208e366ff780094a8bda09cf82f1321dd08
Andreas Naustdal <andreas.naustdal@netlife.com>
2020-06-04 16:38:10 +0200

    add functions for calculating co2km by coach, scooter, plane, train, car

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-module.exports = function helloWorld(kgCO2) {
-  return `Hello world! You entered ${kgCO2} kg CO2`;
-};
+// Source:
+// https://www.energuide.be/en/questions-answers/what-exactly-is-a-tonne-of-co2/2141/
+// Which uses numbers from European Environment Agency
+
+function CO2ToKmTraveledByCoach(kgCO2) {
+  return (100/6.8) * kgCO2;
+}
+
+function CO2ToKmTraveledByScooter(kgCO2) {
+  return (100/7.2) * kgCO2;
+}
+
+function CO2ToKmTraveledByPlanePerPassenger(kgCO2) {
+  return (100/28.5) * kgCO2;
+}
+
+function CO2ToKmTraveledByTrainPerPassenger(kgCO2) {
+  return (100/1.4) * kgCO2;
+}
+
+function CO2ToKmTraveledByCarAlone(kgCO2) {
+  // Driving 100 km alone by car = 10,4 kg CO2/passenger
+
+  return (100/10.4) * kgCO2;
+}
+
+module.exports = {
+  CO2ToKmTraveledByCoach,
+  CO2ToKmTraveledByScooter,
+  CO2ToKmTraveledByPlanePerPassenger,
+  CO2ToKmTraveledByTrainPerPassenger,
+  CO2ToKmTraveledByCarAlone
+}
